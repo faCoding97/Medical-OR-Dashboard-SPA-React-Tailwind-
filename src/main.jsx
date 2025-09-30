45808f26ef7e12cc91c85b4a6605f406f96ee297
faraz <104676843+faCoding97@users.noreply.github.com>
2025-09-30 14:30:02 +0200

    .gitignore
    write .gitignore
    and install react-router-dom
    also add  base: '/demo/' in vite.config.js

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { HashRouter as Router } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>
 );
